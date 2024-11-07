911be2c47f065365b6481eef3612fb4eca3066f6
Luco Bellic <luco.bellic@protonmail.com>
2024-11-07 20:17:46 +0100

    feat(hypr): configure hyprlock

--- nix/wm/hypr.nix
+++ nix/wm/hypr.nix
@@ -4,12 +4,10 @@
 
   home.packages = [
     pkgs.hyprpicker # color picker
-    pkgs.hyprlock
     pkgs.libnotify # for notifications
     pkgs.mako
     pkgs.networkmanager # network manager
     pkgs.grimblast # screenshot tool
-    pkgs.swaylock-effects # lock screen
     pkgs.swww
     pkgs.rofi-wayland
     pkgs.xdg-desktop-portal-hyprland # xdg desktop portal for hyprland
