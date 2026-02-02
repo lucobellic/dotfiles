e037e8285d234c8ba673f2aa3076a9c51b1258cc
Luco Bellic <luco.bellic@protonmail.com>
2026-02-02 16:30:13 +0100

    chore(cursor): add cursor theme and set default to rose poine

--- nix/wm/themes/cursor.nix
+++ nix/wm/themes/cursor.nix
@@ -1,11 +1,18 @@
 { pkgs, ... }:
 {
+  home.packages = with pkgs; [
+    banana-cursor
+    bibata-cursors
+    oreo-cursors-plus
+    rose-pine-cursor
+  ];
+
   home.pointerCursor = {
     gtk.enable = true;
     x11.enable = true;
     hyprcursor.enable = true;
-    name = "Bibata-Modern-Ice";
-    package = pkgs.bibata-cursors;
-    size = 12;
+    name = "BreezeX-RosePineDawn-Linux"; # `~/.nix-profile/share/icons/`
+    package = pkgs.rose-pine-cursor;
+    size = 24;
   };
 }
