d29b3ccfcd258ef0d1b7a46a2ab949c4e3fbaf5f
Luco Bellic <luco.bellic@protonmail.com>
2026-02-04 13:57:42 +0100

    chore(git): update lazygit config and git nix package

--- nix/git.nix
+++ nix/git.nix
@@ -1,56 +1,11 @@
 { pkgs, config, ... }:
 
 {
+  home.packages = with pkgs; [
+    lazygit
+    git
+    delta
+  ];
 
-  home.packages = with pkgs; [ lazygit ];
-
-  xdg.configFile.lazygit.source =
-    config.lib.file.mkOutOfStoreSymlink ../config/lazygit;
-
-  programs = {
-    git = {
-      enable = true;
-      settings = {
-        rebase = { autoStash = true; };
-        rerere = { enabled = true; };
-        pull = { rebase = true; };
-        push = { autoSetupRemote = true; };
-      };
-    };
-
-    delta = {
-      enable = true;
-      options = {
-        features = "ayu-gloom";
-        ayu-gloom = {
-          dark = true;
-          commit-style = "normal";
-          commit-decoration-style = "ul";
-          file-decoration-style = "box blue";
-          file-style = "blue";
-          file-modified-label = "";
-          hunk-header-style = "omit";
-          line-numbers = true;
-          line-numbers-left-style = "#4D5566";
-          line-numbers-minus-style = "red";
-          line-numbers-plus-style = "green";
-          line-numbers-right-style = "#4D5566";
-          line-numbers-zero-style = "#4D5566";
-          minus-emph-style = "black red";
-          minus-style = "red";
-          plus-emph-style = "black green";
-          plus-style = "green";
-          zero-style = "#4D5566";
-          syntax-theme = "none";
-          merge-conflict-begin-symbol = "-";
-          merge-conflict-end-symbol = "─";
-          merge-conflict-ours-diff-header-style = "purple";
-          merge-conflict-ours-diff-header-decoration-style = "ul";
-          merge-conflict-theirs-diff-header-style = "purple";
-          merge-conflict-theirs-diff-header-decoration-style = "ul ol";
-        };
-        interactive = { keep-plus-minus-markers = false; };
-      };
-    };
-  };
+  xdg.configFile.lazygit.source = config.lib.file.mkOutOfStoreSymlink ../config/lazygit;
 }
